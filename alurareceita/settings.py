109ea3149daf9ac1cea8f3031d6117424b0e93e8
PedroAz <pdegutty@gmail.com>
2022-11-04 13:57:05 -0300

    receitas-app-login

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuarios',
     'pessoas',
     'receita',
     'django.contrib.admin',
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'alurareceita.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'receita/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
