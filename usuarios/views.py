c07c9b1477b63189b6a67db4d1c0579c18dde3c1
PedroAz <pdegutty@gmail.com>
2022-11-05 16:20:51 -0300

    usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib import auth
+from receita.models import Receita
+
 
 def cadastro(request):
     if request.method == 'POST':
@@ -35,11 +37,48 @@ def login(request):
             print("os campos não podem ser nulos")
             return redirect('login')
         print(email, password)
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username = nome, password=password)
+            if user is not None:
+                auth.login(request, user)
+                print('okoko')
         return redirect ('dashboard')
     return render(request, 'usuarios/login.html')
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect ('index')
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        id = request.user.id
+        receitas = Receita.objects.order_by('-date_receita').filter(pessoa=id)
+
+        dados = {
+            'receitas': receitas
+        }
+
+        return render(request, 'usuarios/dashboard.html', dados)
+    else:
+        return redirect('index')
+
+def cria_receita(request):
+    if request.method == 'POST':
+        nome_receita = request.POST['nome_receita']
+        ingredientes = request.POST['ingredientes']
+        modo_preparo = request.POST['modo_preparo']
+        tempo_preparo = request.POST['tempo_preparo']
+        rendimento = request.POST['rendimento']
+        categoria = request.POST['categoria']
+        foto_receita = request.FILES['foto_receita']
+        user = get_object_or_404(User, pk=request.user.id)
+        receita = Receita.objects.create(pessoa=user,
+        nome_receita = nome_receita, ingredientes = ingredientes,
+        modo_preparo = modo_preparo, tempo_preparo = tempo_preparo,
+        rendimento = rendimento, categoria =categoria, foto_receita = foto_receita)
+        receita.save()
+
+        return redirect('dashboard')
+    else:
+        return render(request, 'usuarios/cria_receita.html')
