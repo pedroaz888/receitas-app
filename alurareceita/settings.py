b50ff16bc2a4550d2c19af3721baa1d23426c690
PedroAz <pdegutty@gmail.com>
2023-03-17 15:11:16 -0300

    oknow

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -139,8 +139,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '../templates/static')
-STATIC_ROOT = os.path.join('../templates/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'templates/static')
+STATIC_ROOT = os.path.join('static')
 
 #Media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
