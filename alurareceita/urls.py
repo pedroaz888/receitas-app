fad126c8d2d04667989d493527138653a72c5e45
PedroAz <pdegutty@gmail.com>
2023-03-17 15:32:51 -0300

    url

--- alurareceita/urls.py
+++ alurareceita/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
